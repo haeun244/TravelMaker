72657f03687bd24e002dc22563d062d0dfb38f6e
Haeun Lee <cherrypoco@naver.com>
2018-09-18 14:50:57 +0900

    신상버젼

--- travlemaker/src/main/java/com/acon/dto/Comments.java
+++
@@ -1,8 +0,0 @@
-package com.acon.dto;
-
-import org.apache.ibatis.type.Alias;
-
-@Alias("Comments")
-public class Comments {
-
-}
--- travlemaker/src/main/java/com/acon/dto/Member.java
+++
@@ -1,8 +0,0 @@
-package com.acon.dto;
-
-import org.apache.ibatis.type.Alias;
-
-@Alias("Member")
-public class Member {
-
-}
